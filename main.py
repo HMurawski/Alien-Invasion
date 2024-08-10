c0fe8611741795f66a60968c0f29f169332b194c
Hubert Murawski <h.murawski9@gmail.com>
2024-08-10 22:03:41 +0200

    added button class to start the game, changed starting state

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -28,6 +29,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        #make a play button
+        self.play_button = Button(self, "Play")
 
 
     def run_game(self):
@@ -184,6 +188,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        #draw the play button if the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
 
